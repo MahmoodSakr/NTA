bab89cd7df780a5a89d53141a646dc91e29523bb
Mahmoud Sakr <ma7mouedsakr@gmail.com>
2022-09-14 23:53:23 +0200

    okay

--- Planning/scripts/login.js
+++ Planning/scripts/login.js
@@ -1,10 +1,21 @@
-function login(){
-     var username = document.getElementById('username').value;
-     var password = document.getElementById('password').value;
-     
-     if((username=='adnan'||username=='sakr'||username=='mena'||username=='mayar'||username=='tony') && (password=='sakr')){
-          document.location.href='./htmls/takhtet1.html'
-     }else{
-          alert('Username or Password is incorrect !')
-     }
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username == "admin" && password == "sakr") {
+    document.location.href = "./htmls/selection.html";
+  } else if (
+    (username == "adnan" ||
+      username == "sakr" ||
+      username == "mena" ||
+      username == "mayar" ||
+      username == "tony") &&
+    password == "sakr"
+  ) {
+    document.location.href = "./htmls/takhtet.html";
+  } else if (username == "taimor" && password == "sakr") {
+    document.location.href = "./htmls/tadreb.html";
+  } else {
+    alert("Username or Password is incorrect !");
+  }
 }
